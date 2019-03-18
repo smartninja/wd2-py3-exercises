76405c3dc88b7fa40bcfac0bf70111fef56e2f43
Matej Ramuta <matej.ramuta@gmail.com>
2019-03-18 17:45:16 -0400

    heroku scheduler instead of huey periodic tasks

---
+++ lesson-14/ninja-tech-forum-cron/tasks/scheduler_hello.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print("Hello SmartNinja, this is a scheduled task! :)")
+
+
+if __name__ == '__main__':
+    say_hello()
--- lesson-14/ninja-tech-forum-cron/tasks/scheduler_new_topics.py
+++ lesson-14/ninja-tech-forum-cron/tasks/scheduler_new_topics.py
@@ -1,24 +1,20 @@
 import datetime
-
-from .worker import huey
-from huey import crontab
 from models.user import User
 from models.topic import Topic
 from utils.email_helper import send_email_to_one_recipient
 
 
-@huey.periodic_task(crontab(day_of_week='1', hour='8', minute='0'))  # 1 is Monday
-def new_topics_weekly():
-    # get topics from the previous week
+def new_topics_email():
+    # get topics from yesterday
     topics = Topic.get_topics_date_range(lt=datetime.datetime.now(),  # between today
-                                         gt=(datetime.datetime.now() - datetime.timedelta(days=7)))  # and 7 days ago
+                                         gt=(datetime.datetime.now() - datetime.timedelta(days=1)))  # and 1 day ago
 
     # if no topics, finish the task without sending the email
     if not topics:
-        print("No new topics created last week, so no email will be sent.")
+        print("No new topics created yesterday, so no email will be sent.")
     else:
         # create an email message
-        message = "Topics created in the previous week:\n"
+        message = "Topics created yesterday:\n"
 
         for topic in topics:
             message += "- {0}\n".format(topic.title)
@@ -29,4 +25,8 @@ def new_topics_weekly():
         for user in users:
             if user.email_address:
                 send_email_to_one_recipient(recipient_email=user.email_address,
-                                            subject="See topics from the last week", message=message)
+                                            subject="See new topics at Ninja Tech Forum", message=message)
+
+
+if __name__ == '__main__':
+    new_topics_email()
