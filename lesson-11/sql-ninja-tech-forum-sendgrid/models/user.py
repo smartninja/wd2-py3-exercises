9f6fc1df376ae9d92ce87999c5aaacccbda15f16
Matej Ramuta <matej.ramuta@gmail.com>
2019-05-06 22:25:56 -0400

    lesson 11 sendgrid example

---
+++ lesson-11/sql-ninja-tech-forum-sendgrid/models/user.py
@@ -0,0 +1,11 @@
+from models.settings import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)  # a username must be unique!
+    email_address = db.Column(db.String, unique=True)  # email must be unique!
+    password_hash = db.Column(db.String)
+    session_token = db.Column(db.String)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
