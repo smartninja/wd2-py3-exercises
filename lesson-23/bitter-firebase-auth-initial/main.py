8be3517b118187e5e778846ccb477d3aa43f184c
Matej Ramuta <matej.ramuta@gmail.com>
2019-05-13 22:29:17 -0400

    lesson 23: firebase auth (without backend verification)

---
+++ lesson-23/bitter-firebase-auth-initial/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from handlers.bitts import bitt_handlers
+from models.settings import db
+
+app = Flask(__name__)
+app.register_blueprint(bitt_handlers)
+
+# create tables in a database (important: this does not update tables, only creates new ones)
+db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True, host="localhost", port="5000")
